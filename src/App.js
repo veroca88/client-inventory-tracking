6b10bedb2763bbc3dae97dedf2654db76c20a58f
veroca88 <veronicadonoso1506@gmail.com>
2020-11-12 22:04:04 -0600

    Create context to use in Authentication

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import react, {useState, useContext} from 'react'
 import './App.css';
 import Navbar from './component/Navbar';
 import Authentication from './component/Authentication/Signup'
 import ProtectedResource from './component/Authentication/ProtectedResource'
-// import { Route, Switch } from 'react-router-dom';
+import { AuthContext } from './component/Context/AuthContext';
 
-function App() {
-  return (
-    <div className="App">
-      <Authentication />
-      <ProtectedResource />
-    </div>
-  );
+export default function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const login = () => {
+    setLoggedIn(true);
+}
+const logout = () => {
+    setLoggedIn(false);
 }
 
-export default App;
+const valuesOfContext = AuthContext.Provider
+// const value = useContext(AuthContext)
+  return (
+    <AuthContext.Provider 
+    value={login, logout, console.log("+++++++++++++++", valuesOfContext)}>
+      <Authentication />
+      <ProtectedResource />
+      </AuthContext.Provider>
+  );
+}
